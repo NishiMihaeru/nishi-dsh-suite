422582cfecfcf3e55be443abd1b821eec378c112
NishiMihaeru <130587321+NishiMihaeru@users.noreply.github.com>
2026-08-28 07:41:49 +0400

    refactor(memory): keep transaction coordination internal

--- packages/project-memory/src/index.ts
+++ packages/project-memory/src/index.ts
@@ -1,5 +1,24 @@
 export * from './paths.js'
-export * from './bootstrap.js'
+export {
+  MAX_BOOTSTRAP_LINES,
+  MAX_BOOTSTRAP_BYTES,
+  INITIAL_MEMORY_MD_CONTENT,
+  truncateLines,
+  truncateUtf8Buffer,
+  boundedUtf8Bootstrap,
+  ensureProjectMemoryBootstrap,
+  readProjectMemoryBootstrap,
+  writeProjectMemoryBootstrap,
+  editProjectMemoryBootstrap,
+  insertTopicIntoMemoryMapContent,
+  ensureMemoryMapEntry,
+} from './bootstrap.js'
+export type {
+  EnsureProjectMemoryResult,
+  ReadProjectMemoryResult,
+  WriteProjectMemoryBootstrapResult,
+  EditProjectMemoryBootstrapResult,
+} from './bootstrap.js'
 export * from './topics.js'
 export * from './context.js'
 export * from './init.js'
